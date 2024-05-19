e2309da9074701564cb71c3fb043ed2949d8a9a1
kuRomek <qustes@gmail.com>
2024-05-19 18:38:41 +0400

    refactor: make some small changes in Target.cs

--- Assets/Scripts/Target.cs
+++ Assets/Scripts/Target.cs
@@ -6,16 +6,14 @@ public class Target : MonoBehaviour
     [SerializeField] private float _speed;
 
     private Checkpoint[] _checkpoints;
-    private Checkpoint _currentCheckpoint;
     private int _currentCheckpointIndex;
 
     private void Awake()
     {
         _checkpoints = _checkpointsObject.GetComponentsInChildren<Checkpoint>();
         _currentCheckpointIndex = 0;
-        _currentCheckpoint = _checkpoints[_currentCheckpointIndex];
 
-        transform.forward = _currentCheckpoint.transform.position - transform.position;
+        transform.forward = _checkpoints[_currentCheckpointIndex].transform.position - transform.position;
     }
 
     private void Update()
@@ -25,13 +23,9 @@ public class Target : MonoBehaviour
 
     private void Move()
     {
-        if (transform.position == _currentCheckpoint.transform.position)
-        {
-            _currentCheckpointIndex++;
-            _currentCheckpointIndex %= _checkpoints.Length;
-            _currentCheckpoint = _checkpoints[_currentCheckpointIndex];
-        }
+        if (transform.position == _checkpoints[_currentCheckpointIndex].transform.position)
+            _currentCheckpointIndex = ++_currentCheckpointIndex % _checkpoints.Length;
 
-        transform.position = Vector3.MoveTowards(transform.position, _currentCheckpoint.transform.position, _speed * Time.deltaTime);
+        transform.position = Vector3.MoveTowards(transform.position, _checkpoints[_currentCheckpointIndex].transform.position, _speed * Time.deltaTime);
     }
 }
